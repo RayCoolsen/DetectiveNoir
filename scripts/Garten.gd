dafb5be1c7c4eb59b3f6238b1c9150c5882e087f
princesssaxonia <princesssaxonia@gmail.com>
2024-11-24 10:12:44 +0100

    Garten skript
    Sichtbarkeit von lady und Noir angepasst ( im skript ) (ich weiß aber nicht ob das stimmt / Funktioniert , weil ich nicht herausgefunden habe, wie man das testet) :(

--- scripts/Garten.gd
+++ scripts/Garten.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
+@onready var detektive: Sprite2D = $Detektive
+@onready var ehefrau: Sprite2D = $Ehefrau
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	ehefrau.visible = true
+	detektive.visible = false
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
+func _input(event: InputEvent):
+	if Dialogic.current_timeline != null:
+		return
+		
+	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+		visibility_characters(false)
+		Dialogic.start('Garten')
+		get_viewport().set_input_as_handled()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func visibility_characters(vis: bool):
+	ehefrau.visible = vis
+	detektive.visible = vis
+
+func _on_dialogic_signal(argument: String):
+	if argument == "ende" :
+		visibility_characters(true)
