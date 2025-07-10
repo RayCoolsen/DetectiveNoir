03cf2ec970b81b86734806dfa44401cfe55c8add
Andre Fuchs <raycoolsen@gmx.de>
2025-07-11 01:02:03 +0200

    Connect item_pickup_requested for all children
    Replaces the single connection to item_button with a loop that connects the item_pickup_requested signal for all child nodes. This allows the script to handle item pickup requests from multiple children dynamically.

--- scripts/inventarspieler.gd
+++ scripts/inventarspieler.gd
@@ -1,10 +1,10 @@
 extends Node2D
-@onready var item_button: TextureButton = $Lupe
 
 signal item_pickup_requested(item: Item)
 
 func _ready() -> void:
-	item_button.item_pickup_requested.connect(on_item_pickup_requested)
+	for child in get_children():
+		child.item_pickup_requested.connect(on_item_pickup_requested)
 
 func on_item_pickup_requested(item: Item) -> void:
 	item_pickup_requested.emit(item)
