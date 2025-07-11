d8df0b2fd721a332746b6d3c6c6d748267ca9e30
Andre Fuchs <raycoolsen@gmx.de>
2025-07-11 10:49:14 +0200

    Add inventory UI and input handling
    Introduced a new UIRoot CanvasLayer with an InventoryDialog node to the game scene. Added a ui_root.gd script to handle opening the inventory dialog when the 'inventory' input action is released. Updated project settings to define the 'inventory' input action.

---
+++ scripts/ui_root.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var inventory_dialog: InventoryDialog = %InventoryDialog
+@onready var game: Node2D = $".."
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("inventory"):
+		inventory_dialog.open(game.inventory)
