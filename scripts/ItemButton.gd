f846c41e7ebc2c0e1a841f86be40fbe7512c93ed
Andre Fuchs <raycoolsen@gmx.de>
2025-07-17 18:38:43 +0200

    Add dynamic texture assignment to ItemButton
    Introduces a setter for the collectible_item property in ItemButton.gd to update the button's texture when the item changes, including editor support. Updates Study.tscn to assign textures to Lupe and Suitcase buttons using new Texture2D resources.

--- scripts/ItemButton.gd
+++ scripts/ItemButton.gd
@@ -1,12 +1,25 @@
+@tool
 extends TextureButton
 
-@export var collectible_item: Item
+@export var collectible_item: Item:
+	set(value):
+		collectible_item = value
+		update_texture()
+
 signal item_pickup_requested(item: Item)
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		update_texture()
+	else:
+		if collectible_item and collectible_item.texture:
+			texture_normal = collectible_item.texture
+
+func update_texture() -> void:
 	if collectible_item and collectible_item.texture:
 		texture_normal = collectible_item.texture
-
+	else:
+		texture_normal = null
 
 func _on_pressed() -> void:
 	if Memory.inventory_open:
